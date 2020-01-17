2218e8cf8892ccbb7aca2b3f42e991f88f2d80b0
yanfeiliu <yanfeiliu@tencent.com>
2020-01-17 16:09:52 +0800

    init cmake

---
+++ helloworld.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include "math/MathFunctions.h"
+
+int main(int argc, char *argv[]){
+   std::cout << "Hello World!" << std::endl;
+   power(2,3);
+   add(1,2);
+   Student s;
+   return 0;
+}
